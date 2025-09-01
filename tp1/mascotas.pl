7779266be4e59cfff6402cd8359a299d5a563621
Nicolás Aguero <130182241+Tailz2@users.noreply.github.com>
2025-09-01 00:05:52 +0000

    punto2

---
+++ tp1/mascotas.pl
@@ -0,0 +1,9 @@
+mascota(perro,fido).
+mascota(gato, whiskers).
+mascota(pedro, rex).
+dueño(juan, fido).
+dueño(ana, whiskers).
+dueño(pedro, rex).
+color(fido, marron).
+color(wiskers, gris).
+color(rex, negro).
