1536cb93df18fa7647934efd5ab03457334180d9
Nicolás Aguero <130182241+Tailz2@users.noreply.github.com>
2025-09-04 19:51:25 +0000

    operadores

--- tp2/operadores.pl
+++ tp2/operadores.pl
@@ -7,3 +7,9 @@ divisible_por_5(Stock):- 0 is Stock mod 5.
 area_estante(Lado,Area):- Area is Lado ^ 2.
 es_par(Par) :- 0 is Par mod 2.
 %operadores de comparacion
+es_caro(Precio):- Precio > 50.
+es_barato(Precio):- Precio < 30.
+apto_promocion(Stock) :- Stock >= 10.
+bajo_stock(Stock) :- Stock =< 5.
+precio_correcto(Precio, Base, Imp) :- Precio =:= Base + Imp.
+precios_distintos(Precio1, Precio2) :- Precio1 =\= Precio2.
