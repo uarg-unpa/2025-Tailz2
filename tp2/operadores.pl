cb3d740d4470195e51428557ce5bc7fc4145ef73
Nicolás Aguero <130182241+Tailz2@users.noreply.github.com>
2025-09-04 19:35:18 +0000

    operadores

--- tp2/operadores.pl
+++ tp2/operadores.pl
@@ -1,3 +1,7 @@
 %primero operadores aritmeticos
 precio_con_impuestos(PrecioBase, Impuesto, Total):-Total is PrecioBase + Impuesto.
 precio_con_descuento(PrecioBase, Descuento, Final):- Final is PrecioBase - Descuento.
+costo_total(Copias,PrecioUnitario,Total):- Total is Copias * PrecioUnitario.
+costo_por_persona(Total, Personas, Costo):- Costo is Total / Personas.
+divisible_por_5(Stock):- 0 is Stock mod 5.
+area_estante(Lado,Area):- Area is Lado ^ 2.
