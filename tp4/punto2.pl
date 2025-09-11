6b25ba24b98658a836decd90d3a1daa034e7b679
Nicolás Aguero <130182241+Tailz2@users.noreply.github.com>
2025-09-11 22:29:46 +0000

    punto2

--- tp4/punto2.pl
+++ tp4/punto2.pl
@@ -6,4 +6,7 @@ hijo(maria,juan).
 %a)Relacion ancestro(X,Y) donde X es ancestro de Y
 ancestro(X,Y) :- hijo(X,Y).
 ancestro(X,Y) :-hijo(Y,Z), ancestro(X,Z).
-%b)
+%b)Relacion descendiente
+descendiente(X,Y) :- hijo(X,Y).                    % Caso base
+descendiente(X,Y) :- hijo(X,Z), descendiente(Z,Y). % Caso recursivo
+
