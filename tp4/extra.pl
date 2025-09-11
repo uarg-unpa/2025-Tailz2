ef2f287f3d5dc04ef190bb332eece8ef86f45c7e
Nicolás Aguero <130182241+Tailz2@users.noreply.github.com>
2025-09-11 21:06:30 +0000

    extra

--- tp4/extra.pl
+++ tp4/extra.pl
@@ -2,10 +2,10 @@
 %?Sumar_veces(2,4,R).
 %Caso base
 sumar_veces(0,_,0).
-sumar_veces(X,Y,R):- X =\= 0, suma(X,Y,C,A,R).
+sumar_veces(X,Y,R):- X =\= 0, suma(X,Y,0,0,R).
 %caso base
-suma(X,Y,C,A,R). %cuando y e c sean iguales, termina la operacion.
+%suma(X,Y,Y,A,A). %cuando y e c sean iguales, termina la operacion.
 %otra forma
-%suma(X,Y,C,A,R):- Y =:= C.
+suma(X,Y,C,A,A):- Y =:= C.
 %Caso general
 suma(X,Y,C,A,R):- C < Y, A1 is X+A, C1 is C+1, suma(X,Y,C1,A1,R).
